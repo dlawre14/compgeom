d4edc67d902bd921ecf6a8c5965f9e63dd8b83a2
Dylan Lawrence <dlawre14@slu.edu>
2015-01-15 17:45:55 -0600

    more tweaks to preliminary setup

--- convex_hull.py
+++ convex_hull.py
@@ -18,9 +18,39 @@ parser.add_argument ('-g', action='store_true', default=False, help='add a graph
 parser.add_argument ('-v', action='store_true', default=False, help='add a text output to the program')
 parser.add_argument ('-d', type=float, help='add a delay to the program, -1 is for manual advance, > 0 is for a timed delay')
 
+def monotoneChain(points, listeners):
+  points = sorted(points, key=attrgetter('_x', '_y'))
+  print (points[0])
+
+  edgePoints = []
+  edgePoints.append(points[0])
+  points.pop(0)
+
+  topPoints = []
+  bottomPoints = []
+
+  for p in points:
+    if p.getY() < 200:
+      topPoints.append(p)
+      for l in listeners: l.setPointColor(p, 'blue')
+    else:
+      bottomPoints.append(p)
+      for l in listeners: l.setPointColor(p, 'green')
+
+
+
 args = parser.parse_args()
 
-print (args)
+listeners = []
 
-l = GraphicsListener()
+if args.g:
+  listeners.append(GraphicsListener())
+
 ps = utils.pointsInCircle(150, args.p, (200,200))
+
+for p in ps:
+  for l in listeners:
+    l.pointAdded(p)
+
+if args.algorithm == 'monotone':
+  monotoneChain(ps, listeners)
