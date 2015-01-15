64f39e4d11a872a8c6b18d0fb83593ad564d1542
Dylan Lawrence <dlawre14@slu.edu>
2015-01-15 16:08:17 -0600

    added basic argument parsing for algorithms

--- convex_hull.py
+++ convex_hull.py
@@ -11,13 +11,16 @@ from operator import attrgetter
 import argparse
 
 parser = argparse.ArgumentParser(description='Suite of convex hull algorithms with optional graphical implementation')
-parser.add_argument ('-p', type=int, nargs=1, help='number of points to generate')
-parser.add_argument ('--seed', type=int, nargs=1, help='seed to use for random point generation')
+parser.add_argument ('-p', type=int, help='number of points to generate', required=True)
+parser.add_argument ('--seed', type=int, help='seed to use for random point generation')
+parser.add_argument ('-algorithm', type=str, help='algorithm to use, available options: monotone', required=True)
+parser.add_argument ('-g', action='store_true', default=False, help='add a graphical output to the program')
+parser.add_argument ('-v', action='store_true', default=False, help='add a text output to the program')
+parser.add_argument ('-d', type=float, help='add a delay to the program, -1 is for manual advance, > 0 is for a timed delay')
 
 args = parser.parse_args()
 
+print (args)
+
 l = GraphicsListener()
-ps = utils.pointsInCircle(150, 50, (200,200))
-for p in ps:
-  l.pointAdded(p)
-l.drawPath(ps)
+ps = utils.pointsInCircle(150, args.p, (200,200))
