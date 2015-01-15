ed0f5711da47a655ab49b9021f0ba012a2ecc742
Dylan Lawrence <dlawre14@slu.edu>
2015-01-15 13:56:47 -0600

    more refinements, points apppear to be trivially sortable

--- convex_hull.py
+++ convex_hull.py
@@ -6,9 +6,13 @@ from my_modules.geom.utils import utils as utils
 from my_modules.listeners.graphicslistener import GraphicsListener
 
 from fractions import Fraction
+from operator import attrgetter
 
 l = GraphicsListener()
-p = utils.pointsInCircle(175, 500, (200,200), 5)
+p = utils.pointsInCircle(175, 25, (200,200))
 
 for point in p:
   l.pointAdded(point)
+
+p = sorted(p, key=attrgetter('_x','_y'))
+l.setPointColor(p[0], 'green')
--- my_modules/listeners/graphicslistener.py
+++ my_modules/listeners/graphicslistener.py
@@ -6,10 +6,27 @@ class GraphicsListener(Listener):
   def __init__(self, height=400, width=400):
     self.canv = cs1.Canvas(height,width)
     self.points = []
-    self.graphicalPointObjects = {}
+    self.pointObjects = {}
+    self.lineObjects = {}
 
   def pointAdded(self, point):
     self.points.append(point)
-    self.graphicalPointObjects[point] = cs1.Circle(2, point.tocs1Point())
-    self.graphicalPointObjects[point].setFillColor('black')
-    self.canv.add(self.graphicalPointObjects[point])
+    self.pointObjects[point] = cs1.Circle(5, point.tocs1Point())
+    self.pointObjects[point].setFillColor('black')
+    self.canv.add(self.pointObjects[point])
+
+  def setPointColor(self, point, color):
+    if point in self.points:
+      self.pointObjects[point].setFillColor(color)
+    else:
+      raise RuntimeError('Specified points not in active point set')
+
+  def drawLine(self, p1, p2):
+    '''This draws a line between any two known points'''
+    if p1 in self.points and p2 in self.points:
+      self.lineObjects[(p1,p2)] = cs1.Path(p1.tocs1Point(), p2.tocs1Point())
+      self.lineObjects[(p1,p2)].setBorderWidth(2)
+      self.canv.add(self.lineObjects[(p1,p2)])
+
+    else:
+      raise RuntimeError('Specified points not in active point set')
